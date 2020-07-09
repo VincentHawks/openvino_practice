4db6a4f064c022737381b433fc8af0509f563ce3
VincentHawks <qwertar3@gmail.com>
2020-07-09 20:12:33 +0300

    Reverted aux modules to the original state; played around with K

--- modules/2_mnist/src/mnist.cpp
+++ modules/2_mnist/src/mnist.cpp
@@ -24,6 +24,29 @@ void loadImages(const std::string& filepath,
 
     // TODO: follow "FILE FORMATS FOR THE MNIST DATABASE" specification
     // at http://yann.lecun.com/exdb/mnist/
+	
+	int rows = readInt(ifs);
+	int cols = readInt(ifs);
+
+	Mat image_buffer(rows, cols, CV_8U);
+	uint8_t pixel_buffer;
+	int checksum;
+	for (int i = 0; i < numImages; i++)
+	{
+		checksum = 0;
+		for (int y = 0; y < rows; y++)
+		{
+			for (int x = 0; x < cols; x++)
+			{
+				ifs.read((char*)&pixel_buffer, 1);
+				image_buffer.at<uint8_t>(Point(x, y)) = pixel_buffer;
+				checksum += (int)pixel_buffer;
+			}
+		}
+		assert(checksum != 0 && checksum != 255 * rows * cols);
+		images.push_back(image_buffer);
+	}
+	ifs.close();
 }
 
 void loadLabels(const std::string& filepath,
@@ -38,25 +61,69 @@ void loadLabels(const std::string& filepath,
 
     // TODO: follow "FILE FORMATS FOR THE MNIST DATABASE" specification
     // at http://yann.lecun.com/exdb/mnist/
+
+	unsigned char label_buffer;
+	for (int i = 0; i < numLabels; i++)
+	{
+		ifs.read((char*)&label_buffer, 1);
+		assert(label_buffer > -1 && label_buffer < 10);
+		labels.push_back((int)label_buffer);
+	}
+	ifs.close();
 }
 
 void prepareSamples(const std::vector<cv::Mat>& images, cv::Mat& samples) {
-    CV_Error(Error::StsNotImplemented, "prepareSamples");
+	std::cout << "Preparing samples..." << std::endl;
+	int rows = images.at(0).rows;
+	int cols = images.at(0).cols;
+	samples = Mat(images.size(), rows * cols, CV_8UC1);
+	for (int i = 0; i < images.size(); i++)
+	{
+		for (int y = 0; y < rows; y++)
+		{
+			for (int x = 0; x < cols; x++)
+			{
+				samples.at<uint8_t>(i, rows * y + x) = images[i].at<uint8_t>(Point(x, y));
+			}
+		}
+	}
+	samples.convertTo(samples, CV_32FC3);
 }
 
 Ptr<ml::KNearest> train(const std::vector<cv::Mat>& images,
                         const std::vector<int>& labels) {
-    CV_Error(Error::StsNotImplemented, "train");
+    //CV_Error(Error::StsNotImplemented, "train");
+	std::cout << "Training in progress..." << std::endl;
+	Mat samples;
+	prepareSamples(images, samples);
+	Ptr<ml::KNearest> model = ml::KNearest::create();
+	model->train(samples, ml::ROW_SAMPLE, labels);
+	return model;
 }
 
 float validate(Ptr<ml::KNearest> model,
                const std::vector<cv::Mat>& images,
                const std::vector<int>& labels) {
-    CV_Error(Error::StsNotImplemented, "validate");
+    //CV_Error(Error::StsNotImplemented, "validate");
+	std::cout << "Validation..." << std::endl;
+	Mat samples, results;
+	prepareSamples(images, samples);
+
+	model->findNearest(samples, 7, results);
+
+	float successful_predictions = 0;
+	std::cout << results << std::endl;
+	for (int i = 0; i < results.cols; i++)
+	{
+		if (round(results.at<float>(i)) == (float)labels[i]) successful_predictions++;
+	}
+	return successful_predictions / (float)images.size();
 }
 
 int predict(Ptr<ml::KNearest> model, const Mat& image) {
     // TODO: resize image to 28x28 (cv::resize)
+	Mat test_image;
+	//resize(image, test_image, );
 
     // TODO: convert image from BGR to HSV (cv::cvtColor)
 
